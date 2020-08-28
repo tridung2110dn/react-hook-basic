fecd3f958b05e57e80783b5003aa973d69dc86d9
Dung Nguyen <dung.nguyen.vcr@gmail.com>
2020-08-28 21:35:21 +0700

    Practice TodoList example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from "react";
+import "./App.scss";
+import TodoList from "./component/TodoList";
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: "I love Easy Frontend! 😍" },
+    { id: 2, title: "We love Easy Frontend! 🥰" },
+    { id: 3, title: "They love Easy Frontend! 🚀" },
+  ]);
+
+  function handleTodoClick(todo) {
+    // find index using findIndex
+    const index = todoList.findIndex((x) => x.id === todo.id);
+    // clone state
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className="App">
       <h1> Hello Hook </h1>
+      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
 }
