93d25d8d08830f3b2015ff04165cc0d7638acf4b
Dung Nguyen <dung.nguyen.vcr@gmail.com>
2020-08-29 11:49:33 +0700

    PostList component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.scss";
 import TodoList from "./component/TodoList";
 import TodoForm from "./component/TodoForm";
+import PostList from "./component/PostList";
 
 function App() {
+  // declare state
   const [todoList, setTodoList] = useState([
     { id: 1, title: "I love Easy Frontend! 😍" },
     { id: 2, title: "We love Easy Frontend! 🥰" },
     { id: 3, title: "They love Easy Frontend! 🚀" },
   ]);
 
+  const [postList, setPostList] = useState([]);
+
+  // declare effect
+  useEffect(() => {
+    async function fetchPostList() {
+      const requestUrl =
+        "http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1";
+      const response = await fetch(requestUrl);
+      const responseJSON = await response.json();
+
+      // object destructuring
+      const { data } = responseJSON;
+      setPostList(data);
+    }
+
+    fetchPostList();
+  }, []);
+
+  // declare function
   function handleTodoClick(todo) {
     // find index using findIndex
     const index = todoList.findIndex((x) => x.id === todo.id);
-    if (index < 0) return
+    if (index < 0) return;
     // clone state
     const newTodoList = [...todoList];
     newTodoList.splice(index, 1);
@@ -23,8 +44,8 @@ function App() {
   function handleTodoFormSubmit(formValues) {
     const newTodo = {
       id: todoList.length + 1,
-      ...formValues
-    }
+      ...formValues,
+    };
 
     // clone state
     const newTodoList = [...todoList];
@@ -37,13 +58,11 @@ function App() {
   return (
     <div className="App">
       <h1> Hello Hook </h1>
-      <TodoForm onSubmit={handleTodoFormSubmit}
-      />
-      <TodoList
-        todos={todoList}
-        onTodoClick={handleTodoClick} />
+      {/* <TodoForm onSubmit={handleTodoFormSubmit}/> */}
+      {/* <TodoList todos={todoList} onTodoClick={handleTodoClick} /> */}
+      <PostList posts={postList} />
     </div>
-  )
+  );
 }
 
 export default App;
